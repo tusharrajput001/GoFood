7183427bfceec969865881d2542de663798240c1
tusharrajput001 <devoptushar@gmail.com>
2024-06-07 22:50:21 +0530

    hero and navbar responsive done

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,27 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import './Navbar.css';
 
 const Navbar = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
     return (
         <nav className="navbar">
-                        <div className="navbar-center">
+            <div className="navbar-center">
                 <h1>GO FOOD</h1>
             </div>
-            <div className="navbar-left">
+            <div className={`navbar-left ${menuOpen ? 'open' : ''}`}>
                 <a href="#">Home</a>
                 <a href="#">Groceries</a>
                 <a href="#">Pages</a>
             </div>
-            <a href="#">Connect With Us</a>
-            <div className="navbar-right">
-                
+            <a href="#" className="connect">Connect With Us</a>
+            <div className={`navbar-right ${menuOpen ? 'open' : ''}`}>
                 <div className="icons">
                     <a href="#"><i className="fas fa-search"></i></a>
                     <a href="#"><i className="fas fa-user"></i></a>
                     <a href="#"><i className="fas fa-heart"></i></a>
                     <a href="#"><i className="fas fa-shopping-cart"></i></a>
                 </div>
+            </div>
+            <div className="hamburger" onClick={toggleMenu}>
+                <i className="fas fa-bars"></i>
             </div>
         </nav>
     );
